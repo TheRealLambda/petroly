34ee3fe139bdec3ce19edab8335199ace6c05de3
lambda <lambda@Xubuntu.myguest.virtualbox.org>
2024-07-29 21:27:16 +0300

    a

--- src/components/CoursesPage.jsx
+++ src/components/CoursesPage.jsx
@@ -5,6 +5,7 @@ import axios from "axios"
  const CoursesPage = () => {
 
   const [courses, setCourses] = useState([])
+  const [activeCourses, setActiveCourses] = useState([])
   const [term, setTerm] = useState("")
   const [department, setDepartment] = useState("")
 
@@ -19,14 +20,14 @@ import axios from "axios"
 
   useEffect(() => {
 
-    async function loadCourses() {
+    async function loadActiveCourses() {
       const result = await axios.get("http://localhost:3001/api/active-courses")
       console.log(result.data);
-      setCourses(result.data)
+      setActiveCourses(result.data)
 
 
     } 
-    loadCourses()
+    loadActiveCourses()
 
     const modalContainer = document.getElementById("searchModalContainer")
     modalContainer.addEventListener("scrollend", (e) => {
@@ -36,6 +37,16 @@ import axios from "axios"
       }
     })
   }, [])
+
+  const addCourse = async (course) => {
+    console.log("CCCCCCCCCCCCCCCCCCCCC:", course);
+    const body = {
+      course_id: course._id
+    }
+    const result = await axios.post("http://localhost:3001/api/active-courses", body)
+    console.log(result.data);
+    setActiveCourses(activeCourses.concat(result.data.course_id))
+  }
 
   useEffect(() => {
     const searchCourses = async (e) => {
@@ -94,6 +105,7 @@ import axios from "axios"
                 <div className="bgcolor-white">
                   <p>{course.course_name} {course.course_type}</p>
                   <p>{course.course_term} {course.course_department}</p>
+                  <button onClick={() => addCourse(course)}>Add</button>
                 </div>
               )
             })}
@@ -125,7 +137,7 @@ import axios from "axios"
         <div className="text-14-medium bgcolor-white color-accent">231</div>
       </div>
       <div className="courses">
-        {courses.map(course => <div className="bgcolor-white"></div>)}
+        {courses.map(course => <div className="bgcolor-white">{course.course_name} {course._id}</div>)}
       </div>
       <div onClick={openSearchModal} className="add_button bgcolor-primary">
         <svg className="fillcolor-white" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M440-440v120q0 17 11.5 28.5T480-280q17 0 28.5-11.5T520-320v-120h120q17 0 28.5-11.5T680-480q0-17-11.5-28.5T640-520H520v-120q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640v120H320q-17 0-28.5 11.5T280-480q0 17 11.5 28.5T320-440h120Zm40 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Z"/></svg>
